d80c87b04108a2b69a4a9696f6cb1a2a4dbe9e36
Jonathan Swartz <swartz@pobox.com>
2012-04-26 04:55:55 -0700

    init

---
+++ lib/Dist/Zilla/Plugin/PodWeaverIfPod.pm
@@ -0,0 +1,41 @@
+package Dist::Zilla::Plugin::PodWeaverIfPod;
+use 5.008;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::PodWeaverIfPod - Apply PodWeaver if there is already Pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::PodWeaverIfPod;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::PodWeaverIfPod provides
+
+Questions and feedback are welcome, and should be directed to the mailing list:
+
+    http://groups.google.com/...
+
+Bugs and feature requests will be tracked at RT:
+
+    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Plugin-PodWeaverIfPod
+    bug-dist-zilla-plugin-podweaverifpod@rt.cpan.org
+
+The latest source code can be browsed and fetched at:
+
+    http://github.com/jonswar/perl-dzil-podweaverifpod
+    git clone git://github.com/jonswar/perl-dzil-podweaverifpod.git
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
---
+++ lib/Dist/Zilla/Plugin/PodWeaverIfPod/Util.pm
@@ -0,0 +1,9 @@
+package Dist::Zilla::Plugin::PodWeaverIfPod::Util;
+use strict;
+use warnings;
+use base qw(Exporter);
+
+our @EXPORT_OK = qw(
+);
+
+1;
