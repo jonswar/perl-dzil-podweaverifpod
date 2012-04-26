5417cf6153761390937f77b2afc1dd7971ca6c05
Jonathan Swartz <swartz@pobox.com>
2012-04-26 04:57:12 -0700

    various

--- lib/Dist/Zilla/Plugin/PodWeaverIfPod.pm
+++ lib/Dist/Zilla/Plugin/PodWeaverIfPod.pm
@@ -1,8 +1,24 @@
 package Dist::Zilla::Plugin::PodWeaverIfPod;
-use 5.008;
-use strict;
-use warnings;
 
+use Moose;
+extends qw/ Dist::Zilla::Plugin::PodWeaver /;
+
+around munge_pod => sub {
+    my $inner = shift;
+    my ( $self, $file ) = @_;
+
+    my $content = $file->content;
+
+    if ( $content =~ /=head1/ ) {
+        return $inner->(@_);
+    }
+    else {
+        return;
+    }
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 1;
 
 __END__
@@ -15,27 +31,12 @@ Dist::Zilla::Plugin::PodWeaverIfPod - Apply PodWeaver if there is already Pod
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::PodWeaverIfPod;
+In your L<Dist::Zilla> C<dist.ini>:
+
+    [PodWeaverIfPod]
 
 =head1 DESCRIPTION
 
-Dist::Zilla::Plugin::PodWeaverIfPod provides
-
-Questions and feedback are welcome, and should be directed to the mailing list:
-
-    http://groups.google.com/...
-
-Bugs and feature requests will be tracked at RT:
-
-    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Plugin-PodWeaverIfPod
-    bug-dist-zilla-plugin-podweaverifpod@rt.cpan.org
-
-The latest source code can be browsed and fetched at:
-
-    http://github.com/jonswar/perl-dzil-podweaverifpod
-    git clone git://github.com/jonswar/perl-dzil-podweaverifpod.git
-
-=head1 SEE ALSO
-
-L<Some::Module>
+Dist::Zilla::Plugin::PodWeaverIfPod will only PodWeaver a .pm if there appears
+to be Pod there (i.e. a C<=head1> section).
 
--- lib/Dist/Zilla/Plugin/PodWeaverIfPod/Util.pm
+++
@@ -1,9 +0,0 @@
-package Dist::Zilla::Plugin::PodWeaverIfPod::Util;
-use strict;
-use warnings;
-use base qw(Exporter);
-
-our @EXPORT_OK = qw(
-);
-
-1;
